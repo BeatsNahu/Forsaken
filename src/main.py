b6088aad0ff7517462717662dc37e78eea715334
Nahuel <2007alexisnahuel@gmail.com>
2025-10-03 20:59:33 +0200

    Merge

--- src/main.py
+++ src/main.py
@@ -11,17 +11,17 @@ def main():
     # Main loop
     while running:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            game.handle_event(event)
+            if event.type == pygame.QUIT: # pygame.QUIT event means the user clicked the x
+                running = False # So the loop will stop running and finish the game
+            game.handle_event(event) # Handle other events like keyboard
             
         # Update game state
-        game.update()
+        game.update() 
 
         # Screen rendering
         screen.fill((0, 0, 0))
         game.draw(screen)
-        pygame.display.flip()
+        pygame.display.flip() # Display the updated screen
 
         # Control frame rate
         clock.tick(60)
