27eb380286c04680b775f5ba049f8712314c3ddd
Nahuel <2007alexisnahuel@gmail.com>
2025-10-08 21:01:27 +0200

    Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene

--- src/engine.py
+++ src/engine.py
@@ -6,6 +6,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import pygame
 import os
 <<<<<<< HEAD
@@ -1020,6 +1021,9 @@ class Engine:
 =======
 class Engine:
 >>>>>>> 9dd9a3f (Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene)
+=======
+class Engine:
+>>>>>>> 9dd9a3f (Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene)
     def __init__(self, name):
         self.name = name
 
@@ -1055,6 +1059,7 @@ class Engine:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         print(f"{self.name} engine stopped.")
 >>>>>>> 9dd9a3f (Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene)
 =======
@@ -1274,3 +1279,6 @@ class Engine:
                 if e.get("battle_module"):
                     self.scene_manager.load_scene(e.get("battle_module"))
 >>>>>>> 8530cab ( implement transition manager and refactor scene loading with image caching)
+=======
+        print(f"{self.name} engine stopped.")
+>>>>>>> 9dd9a3f (Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene)
---
+++ src/scene_manager.py
@@ -0,0 +1,10 @@
+import pygame
+import menu  # Import the menu module
+
+class SceneManager: # Create a Game class to manage scene state
+    def __init__(self):
+        self.engine = engine
+        self.current_scene = None
+
+    def load_scene(self, scene_name):
+    scene_maganer.load_scene("Menu") # Load the menu scene
