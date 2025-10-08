6cbc4e897200587d23e8eaa28d08b7e115e97ca7
Nahuel <2007alexisnahuel@gmail.com>
2025-10-08 21:01:27 +0200

    Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene

--- src/engine.py
+++ src/engine.py
@@ -2,6 +2,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 import pygame
 import os
 <<<<<<< HEAD
@@ -686,6 +687,9 @@ class Engine:
 =======
 class Engine:
 >>>>>>> 9dd9a3f (Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene)
+=======
+class Engine:
+>>>>>>> 9dd9a3f (Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene)
     def __init__(self, name):
         self.name = name
 
@@ -719,6 +723,7 @@ class Engine:
 
     def stop(self):
 <<<<<<< HEAD
+<<<<<<< HEAD
         print(f"{self.name} engine stopped.")
 >>>>>>> 9dd9a3f (Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene)
 =======
@@ -828,3 +833,6 @@ class Engine:
                 if e.get("battle_module"):
                     self.scene_manager.load_scene(e.get("battle_module"))
 >>>>>>> 8530cab ( implement transition manager and refactor scene loading with image caching)
+=======
+        print(f"{self.name} engine stopped.")
+>>>>>>> 9dd9a3f (Editing the main - engine - escene_maganer files and configuring them and eliminating extra files. I'm trying to do the first escene)
