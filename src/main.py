e6242d46de29270c964c5c6362b2fa26a0626da1
Alexis <2007alexisnahuel@gmail.com>
2025-10-09 15:02:37 +0200

    Completion of main.py and editing the menu.py file

--- src/main.py
+++ src/main.py
@@ -1,29 +1,42 @@
 import pygame
 from engine import Engine
 from scene_manager import SceneManager
+from scene import Scene
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1080, 720))
     pygame.display.set_caption("Forsaken")
     clock = pygame.time.Clock()
-    running = True
 
+    engine = Engine()
+    engine.screen = screen
+    engine.clock = clock
+
+    escene_manager = SceneManager(engine, Scene)
+    engine.scene_manager = escene_manager
+
+    escene_manager.load_scene("src.scripts.menu") # Load the menu scene
+
+
+    running = True
     # Main loop
-    while running:
+    while running and not getattr(engine, "quit_flag", False):
+        dt = clock.tick(60) / 1000.0  # Delta time in seconds
         for event in pygame.event.get(): # Event handling loop
-            Engine.handle_event(event) # Handle other events like keyboard
+            if event.type == pygame.QUIT:
+                running = False
+                break  
+            SceneManager.handle_event(event) # Handle other events like keyboard
             
         # Update game state
-        Engine.update() 
+        SceneManager.update(dt)
 
         # Screen rendering
-        screen.fill((0, 0, 0))
-        Engine.draw(screen)
+        SceneManager.draw(screen)
         pygame.display.flip() # Display the updated screen
 
         # Control frame rate
-        clock.tick(60)
 
     # Finalize
     pygame.quit()
