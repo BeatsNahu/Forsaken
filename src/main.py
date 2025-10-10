5b2e0a0620ff600af0d5623102690cd9db3dff87
Nahuel <2007alexisnahuel@gmail.com>
2025-10-10 20:57:24 +0200

    Main.py - scene:manager and main_menu are working

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from engine import Engine
 from scene_manager import SceneManager
 from scene import Scene
 
+
 def main():
     # Screen setup
     pygame.init()
@@ -10,7 +11,7 @@ def main():
     pygame.display.set_caption("Forsaken")
     clock = pygame.time.Clock()
 
-    # Wtih this part we avoid repeated code in every scene, because all scenes will have access to the escreen and clock initialized in "Screen setup"
+    # With this part we avoid repeated code in every scene, because all scenes will have access to the escreen and clock initialized in "Screen setup"
     engine = Engine() # Create an instance of the Engine
     engine.screen = screen # Assign the screen to the engine
     engine.clock = clock # Assign the clock to the engine
@@ -18,7 +19,7 @@ def main():
     escene_manager = SceneManager(engine, Scene) # Create an instance of the SceneManager using the Engine and Scene classes
     engine.scene_manager = escene_manager # Assign the scene manager to the engine 
 
-    escene_manager.load_scene("scripts.menu") # Load the initial scene, in this case the menu scene
+    escene_manager.load_scene('scripts.main_menu') # Load the initial scene, in this case the menu scene
 
     # Main game loop
     running = True
@@ -28,13 +29,12 @@ def main():
             if event.type == pygame.QUIT: # and if one is an event of type QUIT, we will stop the loop
                 running = False # Running is set to False and the loop will 
                 break  # Exit the event loop
-        SceneManager.handle_event(event) # Handle other events like keyboard
             
         # Update game state
-        SceneManager.update(dt)
+        escene_manager.update(dt) # The update method of the scene manager will call the update method of the current scene, passing the delta time as argument
 
         # Screen rendering
-        SceneManager.draw(screen) # The draw method of the scene manager will call the draw method of the current scene
+        escene_manager.draw(screen) # The draw method of the scene manager will call the draw method of the current scene
         pygame.display.flip() # Display the updated screen
 
         # Control frame rate
--- temporal.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-from pathlib import Path
-print("PWD:", os.getcwd())
-print("sys.path[0]:", sys.path[0])
-print("sys.path (head):", sys.path[:3])
-print("Archivo main.py en:", Path(__file__).resolve())
