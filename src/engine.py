df212d9a766d02fcd53610921162c7874deb8993
Alexis <2007alexisnahuel@gmail.com>
2025-10-10 16:09:12 +0200

    Run and debug

--- src/engine.py
+++ src/engine.py
@@ -1,6 +1,6 @@
 class Engine:
-    def __init__(self, name): # Constructor of the class
-        self.name = Forsaken # Name of the engine
+    def __init__(self): # Constructor of the class
+        self.name = None # Name of the engine
         self.scene_manager = None # Scene manager of the engine
         self.screen = None # Screen of the engine
         self.clock = None # Clock of the engine
