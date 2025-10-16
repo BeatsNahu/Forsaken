e0ee0c356d753e785e288610a93c9999c3465513
Nahuel <2007alexisnahuel@gmail.com>
2025-10-16 20:57:49 +0200

    Finish the window menu, we need to can select one option and swap the scene or exit the game

--- src/scene.py
+++ src/scene.py
@@ -668,6 +668,7 @@ class Scene:
         self._line_index = 0
 
     def enter(self): pass
+        
     def exit(self): pass
 
     def handle_event(self, event):
@@ -680,7 +681,6 @@ class Scene:
             self._line_index += 1
         else:
             if self.choices:
-                # abrir UI para elegir (delegar a engine.ui o implementar directo)
                 pass
             elif "next" in self.data:
                 self.apply_effects(self.data.get("effects", []))
