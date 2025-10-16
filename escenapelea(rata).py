9352e262a9d8e43eaf4cfa0c460c8e3dc3462fae
Antonio <tgarciamolinet@gmail.com>
2025-10-16 20:42:10 +0200

    hice que en pelea rata sea la pelea de todos los bichos

--- escenapelea(rata).py
+++ escenapelea(rata).py
@@ -2,7 +2,6 @@ import pygame
 import sys
 import programacionleerteclas
 
-vidarata=10
 vidapersonage=10
 
 turno="jugador"
@@ -10,38 +9,50 @@ turno="jugador"
 pygame.init()
 pantalla = pygame.display.set_mode((400, 300))     #hay que borrarlo
 pygame.display.set_caption("Menu principal")
-
+pelea="rata"
 escenario = 0
-
-while True:
-    if turno=="rata":
-        if vidapersonage >=5:
-            vidapersonage -= 3
-            print(vidapersonage)
-            turno="jugador"
-        elif vidapersonage <=4:
-            vidapersonage -= 1
-            print(vidapersonage)
-            turno="jugador"
-    if turno=="jugador":
-        resultado = programacionleerteclas.leer_eventos()
-        if ((resultado is not None) and (escenario == 0)) and ((not (vidapersonage<=0)) or (not (vidarata<=0))):       #si resultado es algo y escenario es 0
-            if resultado == "opcion1":                  #si el jugador selecciona opcion 1
-                print("Opción puñetazo seleccionada")
-                vidarata -= 4
-                vidapersonage -=2
-                turno="rata"
-            elif resultado == "opcion2":               #si el jugador selecciona opcion 2
-                print("Opción patada seleccionada")
-                vidarata -= 2
-                turno="rata"
-        elif vidapersonage <=0:
-            print("Has perdido")
-            pygame.quit()
-            sys.exit()
-        elif vidarata <=0:
-            print("Has ganado")
-            pygame.quit()
-            sys.exit()
-    pantalla.fill((30, 30, 30))
-    pygame.display.flip()
+def pelea(monstruo):
+    while True:
+        if monstruo=="rata":
+            vidamonstruo=10
+            dañofuerte=3
+            dañodebil=1
+        elif monstruo=="rana":
+            vidamonstruo=11
+            dañofuerte=3
+            dañodebil=1
+        elif monstruo=="señorcuchillo":
+            vidamonstruo=12
+            dañofuerte=1
+            dañodebil=4
+        if turno=="monstruo":
+            if vidapersonage >=5:
+                vidapersonage -= dañofuerte
+                print(vidapersonage)
+                turno="jugador"
+            elif vidapersonage <=4:
+                vidapersonage -= dañodebil
+                print(vidapersonage)
+                turno="jugador"
+        if turno=="jugador":
+            resultado = programacionleerteclas.leer_eventos()
+            if ((resultado is not None) and (escenario == 0)) and ((not (vidapersonage<=0)) or (not (vidamonstruo<=0))):       #si resultado es algo y escenario es 0
+                if resultado == "opcion1":                  #si el jugador selecciona opcion 1
+                    print("Opción puñetazo seleccionada")
+                    vidamonstruo -= 4
+                    vidapersonage -=2
+                    turno="monstruo"
+                elif resultado == "opcion2":               #si el jugador selecciona opcion 2
+                    print("Opción patada seleccionada")
+                    vidamonstruo -= 2
+                    turno="monstruo"
+            elif vidapersonage <=0:
+                print("Has perdido")
+                pygame.quit()
+                sys.exit()
+            elif vidarata <=0:
+                print("Has ganado")
+                pygame.quit()
+                sys.exit()
+        pantalla.fill((30, 30, 30))
+        pygame.display.flip()
