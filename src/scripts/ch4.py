5dae11c093757ac4255547c83f888bd9cfacbdd8
Antonio <tgarciamolinet@gmail.com>
2025-10-20 18:45:10 +0200

    estoy poniendo estructura capitulos

---
+++ src/scripts/ch4.py
@@ -0,0 +1,18 @@
+SCENE = {
+    "id": "ch0_intro",
+    "title": "Chapter 0: The Beginning",
+    "background": "assets.backgrounds.Jail.png",
+    "lines": [
+        {"speaker": "Narrator", "text": "En una esquina ves una rana."},
+    ],
+    "choises": [
+        {
+            "text": "Acariciar.",
+            "target": "scripts.ch0_option1",
+        },
+        {
+        "text": "Darle un patada.",
+        "target": "scripts.ch0_option2",
+        }
+    ]
+}
