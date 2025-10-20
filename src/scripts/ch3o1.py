d344e1f1628418f41de324eae70ee311cc2dd9c3
Antonio <tgarciamolinet@gmail.com>
2025-10-20 18:45:10 +0200

    estoy poniendo estructura capitulos

---
+++ src/scripts/ch3o1.py
@@ -0,0 +1,8 @@
+SCENE = {
+    "id": "ch3_pelearata",
+    "title": "Chapter 0: The Beginning",
+    "background": "assets.backgrounds.Jail.png",
+    "lines": [
+        {"speaker": "Narrator", "text": "Te acercas y ves en la esquina varias latas de comida, pero al acercarte aparece una rata mutante."},
+    ]
+}
