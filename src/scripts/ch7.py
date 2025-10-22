0837a60afeb21d091459ec1986cebfcdc86417f8
Antonio <tgarciamolinet@gmail.com>
2025-10-22 20:57:18 +0200

    Remove unused Pygame scripts to clean up the project  y hice un poco de camino de elecciones

---
+++ src/scripts/ch7.py
@@ -0,0 +1,15 @@
+SCENE = {
+    "id": "ch0_intro",
+    "title": "Chapter 0: The Beginning",
+    "background": "assets.backgrounds.Jail.png",
+    "lines": [
+        {"speaker": "Narrator", "text": "El señor te atrapa y empieza a torturarte."},
+        {"speaker": "Narrator", "text": "Piensas que vas a morir, pero no parece que tengas razón, ya que desde eso han pasado meses..."},
+    ],
+    "choises": [
+        {
+            "text": "Option 1",
+            "target": "scripts.ch0_option1",
+        }
+    ]
+}
