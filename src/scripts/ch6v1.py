84a6e4f60a6bfd3bb39d8a678d9ae69119730101
Antonio <tgarciamolinet@gmail.com>
2025-10-22 20:57:18 +0200

    Remove unused Pygame scripts to clean up the project  y hice un poco de camino de elecciones

---
+++ src/scripts/ch6v1.py
@@ -0,0 +1,18 @@
+SCENE = {
+    "id": "ch0_intro",
+    "title": "Chapter 0: The Beginning",
+    "background": "assets.backgrounds.Jail.png",
+    "lines": [
+        {"speaker": "Narrator", "text": "Escuchas un sonido metálico y de que alguien se acerca.."},
+    ],
+    "choises": [
+        {
+            "text": "Pelear.",
+            "target": "scripts.ch0_option1",
+        },
+        {
+        "text": "Huir.",
+        "target": "scripts.ch7",
+        }
+    ]
+}
