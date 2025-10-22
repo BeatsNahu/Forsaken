5fde093dc9bb47284853b17e2b89c25eb7da8445
Antonio <tgarciamolinet@gmail.com>
2025-10-22 20:57:18 +0200

    Remove unused Pygame scripts to clean up the project  y hice un poco de camino de elecciones

---
+++ src/scripts/ch5o1.py
@@ -0,0 +1,19 @@
+SCENE = {
+    "id": "ch5_finalenvenenado",
+    "title": "Chapter 0: The Beginning",
+    "background": "assets.backgrounds.Jail.png",
+    "lines": [
+        {"speaker": "Narrator", "text": "Era venenosa y mueres envenenado."},
+        {"speaker": "Narrator", "text": "BBBBBBBb."},
+    ],
+    "choises": [
+        {
+            "text": "Option 1",
+            "target": "scripts.ch0_option1",
+        },
+        {
+        "text": "Option 2",
+        "target": "scripts.ch0_option2",
+        }
+    ]
+}
