c399a8325e85fdfe370f31a1a3e5d397c01e0cf6
Antonio <tgarciamolinet@gmail.com>
2025-10-24 20:50:07 +0200

    feat: add game icon and update battle scene structure

--- src/scripts/ch5_peleapivecuchillo.py
+++ src/scripts/ch5_peleapivecuchillo.py
@@ -5,6 +5,7 @@ SCENE = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     "player": {                     	# parámetros opcionales de apoyo
   	  "party": ["hero","companion"],
@@ -17,6 +18,8 @@ SCENE = {
   	  "party": ["hero","companion"],
   	  "initial_hp": null          	# null = usar stats actuales del engine
 >>>>>>> 83239ec (feat: add battle scene structure and dialogue for new encounters)
+=======
+>>>>>>> 715058c (feat: add game icon and update battle scene structure)
     },
     "enemies": [
   	  {"id":"pivecuchillo","type":"pivecuchillo"},
@@ -37,6 +40,7 @@ SCENE = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     }
 =======
   }
@@ -49,3 +53,6 @@ SCENE = {
   }
 }
 >>>>>>> 83239ec (feat: add battle scene structure and dialogue for new encounters)
+=======
+    }
+>>>>>>> 715058c (feat: add game icon and update battle scene structure)
