83239ec988fca11c42e5c592703f47df291d8c95
Antonio <tgarciamolinet@gmail.com>
2025-10-24 20:29:59 +0200

    feat: add battle scene structure and dialogue for new encounters

---
+++ src/scripts/ch6_crias.py
@@ -0,0 +1,15 @@
+SCENE = {
+    "id": "ch0_intro",
+    "title": "Chapter 0: The Beginning",
+    "background": "assets.backgrounds.Jail.png",
+    "lines": [
+        {"speaker": "Narrator", "text": "La rana te saca los ojos y pone sus huevos en tu cerebro."},
+        {"speaker": "Narrator", "text": "Ahora formas parte de una familia por suerte o desgracia."},
+    ],
+    "choises": [
+        {
+            "text": "volver a intentar",
+            "target": "scripts.ch1_intro",
+        }
+    ]
+}
