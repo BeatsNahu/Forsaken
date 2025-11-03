458043124fb63de6edd69daefee4db1218086429
Nahuel <2007alexisnahuel@gmail.com>
2025-11-03 18:24:21 +0100

    Error Commit

--- src/scripts/ch6_crias.py
+++ src/scripts/ch6_crias.py
@@ -13,6 +13,7 @@ SCENE = {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             "text": "volver a intentarlo",
 =======
             "text": "volver a intentar",
@@ -29,6 +30,9 @@ SCENE = {
 =======
             "text": "volver a intentar",
 >>>>>>> 83239ec (feat: add battle scene structure and dialogue for new encounters)
+=======
+            "text": "volver a intentarlo",
+>>>>>>> af17f7a (Error Commit)
             "target": "scripts.ch1_intro",
         }
     ]
