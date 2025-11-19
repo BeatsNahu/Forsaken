5c888c7f7ffadbcbc57c02e5c191bd6f24f71239
Antonio <tgarciamolinet@gmail.com>
2025-11-19 21:04:43 +0100

    he corregido el cap 1

--- src/scenes/ch1_intro.py
+++ src/scenes/ch1_intro.py
@@ -6,12 +6,12 @@ SCENE = {
     "volume": 0.1,
     "sfx_on_enter": None, 
     "lines": [
-        {"speaker": "Narrador", "text": "Abres los ojos y te encuentras en un lugar que no reconoces,"},
-        {"speaker": "Narrador", "text": "tratas de recordar como o cuando llegaste aqui,"},
-        {"speaker": "Narrador", "text": "pero cuanto mas lo intentas,"},
+        {"speaker": "Narrador", "text": "Abres los ojos y te encuentras en un lugar que no reconoces;"},
+        {"speaker": "Narrador", "text": "tratas de recordar cómo o cuándo llegaste aquí,"},
+        {"speaker": "Narrador", "text": "pero cuanto más lo intentas,"},
         {"speaker": "Narrador", "text": "te vas dando cuenta de que no eres capaz de recordar nada,"},
-        {"speaker": "Narrador", "text": "nisiquiera tu propio nombre."},
-        {"speaker": "Narrador", "text": "La situación te hace sentir un escalofrio."}
+        {"speaker": "Narrador", "text": "ni siquiera tu propio nombre."},
+        {"speaker": "Narrador", "text": "La situación te hace sentir un escalofrío."}
     ],
     "choices": [
         {
